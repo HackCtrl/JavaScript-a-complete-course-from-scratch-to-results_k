b860d40c496e0a01fd5ea60c639c731fde19c858
HackCtrl <kirill.ivanov08061@gmail.com>
2025-04-08 10:27:28 +0300

    cd

---
+++ lesson 1/Connecting JS to a page and variables.js
@@ -0,0 +1,16 @@
+var leftBorderWidth = 1; 
+{
+    let second = 2; 
+}
+
+const pi = 3.14; 
+
+console.log(second);
+
+// var number = 5; 
+// var string = "Hello"; 
+// var sym = Symbool(); 
+// var boolean = true; 
+// null; 
+// undefined; 
+// var obj = {}; 
